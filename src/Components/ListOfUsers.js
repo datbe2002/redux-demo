a10b9d33b4396a9d43ff1201abe8401696b2664a
TanDat <datttse161294@gmail.com>
2022-10-15 11:26:49 +0700

    install MUI

---
+++ src/Components/ListOfUsers.js
@@ -0,0 +1,34 @@
+const UsersData = [
+    {
+        id: 1,
+        name: "Tan Dat",
+        username: "Dat",
+    },
+    {
+        id: 2,
+        name: "Ho Giang",
+        username: "giang",
+    },
+    {
+        id: 3,
+        name: "Joe Nguyen",
+        username: "joe",
+    },
+    {
+        id: 4,
+        name: "Kevin Khang",
+        username: "kevin",
+    },
+    {
+        id: 5,
+        name: "Kane Le",
+        username: "kane",
+    },
+    {
+        id: 6,
+        name: "Bin Pham",
+        username: "bin",
+    },
+
+];
+export default UsersData;
